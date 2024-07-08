d948fc3230259ebf61fa165f71e9419630fcb889
tatsukawa <tatsukawatakamasa@gmail.com>
2024-07-08 23:46:24 +0900

    最終版

--- spec/system/tasks_spec.rb
+++ spec/system/tasks_spec.rb
@@ -8,12 +8,14 @@ RSpec.describe 'タスク管理機能', type: :system do
          #Task new.(title: '書類作成', content: '企画書を作成する。')
          #FactoryBot.create(:task)
          # タスク一覧画面に遷移
-         visit new_tasks_path
-         fill_in 'task[title]', with: "こんにtは"
-         click_button "作成"
+         visit new_task_path
+         fill_in 'task[title]', with: "こんにちは"
+         fill_in 'task[content]', with:"こんばんは" 
+         click_button "Create Task"
 
-         # visit（遷移）したpage（この場合、タスク一覧画面）に"書類作成"という文字列が、have_content（含まれていること）をexpect（確認・期待）する
+         # 新規投稿pageにvisit（遷移）して"こんにちは"という文字列を、fill_in（記入）してボタンをクリックし、expect（確認・期待）する
          expect(page).to have_content 'こんにちは'
+         expect(page).to have_content 'こんばんは'
          # expectの結果が「真」であれば成功、「偽」であれば失敗としてテスト結果が出力される
       end
     end
@@ -46,9 +48,7 @@ RSpec.describe 'タスク管理機能', type: :system do
         @task4 = Task.create!(title: '書類作成４', content: '企画書を作成する。４')
         FactoryBot.create(:task)
         # タスク詳細画面に遷移（特定）
-        visit task_path(@task3)
-        binding.irb 
-        # 登録ずみのタスクの詳細があるか確認
+        visit task_path(@task3)        # 登録ずみのタスクの詳細があるか確認
         expect(page).to have_content '書類作成３'
         expect(page).to have_content '企画書を作成する。３'
         #他のタスク詳細画面に遷移
