b62d8925b9865be63f74d26419fff5df26c447c8
tatsukawa <tatsukawatakamasa@gmail.com>
2024-07-24 10:32:15 +0900

    確認4−３

--- db/migrate/20240719044040_add_user_id_to_tasks.rb
+++ db/migrate/20240719044040_add_user_id_to_tasks.rb
@@ -1,5 +1,5 @@
 class AddUserIdToTasks < ActiveRecord::Migration[6.1]
   def change
-    add_reference :tasks, :user, foreign_key: true, null: false
+    add_reference :tasks, :user, foreign_key: true, null: true
   end
 end
--- db/migrate/20240723220126_change_user_id_on_tasks_to_not_null.rb
+++
@@ -1,5 +0,0 @@
-class ChangeUserIdOnTasksToNotNull < ActiveRecord::Migration[6.1]
-  def change
-    change_column_null :tasks, :user_id, false
-  end
-end
