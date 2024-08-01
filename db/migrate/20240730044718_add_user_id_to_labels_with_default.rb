bba8c148958910b5f0b6792fc0de91c4a7e7fbe4
tatsukawa <tatsukawatakamasa@gmail.com>
2024-08-01 16:33:48 +0900

    確認5−２

--- db/migrate/20240730044718_add_user_id_to_labels_with_default.rb
+++ db/migrate/20240730044718_add_user_id_to_labels_with_default.rb
@@ -5,11 +5,6 @@ class AddUserIdToLabelsWithDefault < ActiveRecord::Migration[6.1]
 
     # Set a default user_id for existing labels (assuming user_id 1 exists)
     # Adjust this as necessary for your application
-    default_user_id = User.first.id
-    Label.update_all(user_id: default_user_id)
-
-    # Change the column to NOT NULL
-    change_column_null :labels, :user_id, false
   end
 
   def down
--- db/schema.rb
+++ db/schema.rb
@@ -19,7 +19,7 @@ ActiveRecord::Schema.define(version: 2024_07_30_052327) do
     t.string "name", null: false
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.bigint "user_id", null: false
+    t.bigint "user_id"
     t.index ["user_id"], name: "index_labels_on_user_id"
   end
 
